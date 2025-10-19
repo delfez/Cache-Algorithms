3878ddccb52434edb130eff42b2aa68875663fee
delfez <delfez37@protonmail.com>
2025-10-19 17:01:27 -0300

    Simplificacao do algoritmo MRU

--- src/MRUCache.java
+++ src/MRUCache.java
@@ -2,36 +2,42 @@ import java.util.*;
 
 public class MRUCache<K, V> {
     private final int capacidade; //capacidade do mapa
-    private final Map<K, V> mapa; //mapa que guarda a chave e o valor guardado
-    private final Deque<K> maisRecente; //pilha que armazena o valor usado mais recentemente
+    private final Set<K> mapa; //mapa que guarda a chave tipo K e o valor tipo V
+    private final Deque<K> pilha; //interface "double ended queue" de fila com duas pontas. Guarda valores do tipo K
 
-    public MRUCache(int capacity) {
-        this.capacidade = capacity;
-        this.mapa = new HashMap<>();
-        this.maisRecente = new ArrayDeque<>();
+    public MRUCache(int capacidade) {
+        this.capacidade = capacidade;
+        this.mapa = new HashSet<>();
+        this.pilha = new ArrayDeque<>(); //implementação de array dequeue de tamanho flexivel
     }
 
-    public Map<K, V> getMapa() {
+    public Set<K> getMapa() {
         return mapa;
     }
 
-    public void inserir(K key, V value) {
-        if (mapa.containsKey(key)) {
-            maisRecente.remove(key);
-        } else if (mapa.size() == capacidade) {
-            K mostRecent = maisRecente.removeLast();
-            mapa.remove(mostRecent);
-        }
-        mapa.put(key, value);
-        maisRecente.addLast(key);
-    }
+    public void acessar(K key) {
 
-    public V acessar(K key) {
-        if (!mapa.containsKey(key)) return null;
-        maisRecente.remove(key);
-        maisRecente.addLast(key);
-        return mapa.get(key);
+        //caso 1: pagina esta na memoria
+        if (mapa.contains(key)){
+            pilha.remove(key);
+            pilha.addLast(key);
+            return;
+        }
+
+        //caso 2: pagina nao esta na memoria e ela esta cheia
+        if (mapa.size() == capacidade){
+
+            K remover = pilha.removeLast();
+            mapa.remove(remover);
+
+        }
+
+        mapa.add(key);
+        pilha.addLast(key);
+
     }
 
 
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        MRUCache<Integer, String> cache = new MRUCache<>(3);
-        cache.inserir(1, "A");
-        cache.inserir(2, "B");
-        cache.inserir(3, "C");
-        cache.acessar(2);
-        cache.inserir(4, "D");
-        System.out.println(cache.getMapa());
+
+
+
+
     }
 }
 
